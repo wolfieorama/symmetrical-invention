094138b03a304ef10ab4bcaeea4a212af506263f
wolfieorama <wachirawamunyi@gmail.com>
2016-03-31 17:14:23 +0300

    associated teams with employees and managers

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ActiveRecord::Base
   has_many :subordinates, class_name: "Employee", foreign_key: "manager_id"
   belongs_to :manager, class_name: "Employee"
+
+  belongs_to :sec_team, class_name: "Team"
+  belongs_to :ter_team, class_name: "Team"
+
+  has_one :team, foreign_key: "manager_id"
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,11 @@
 class Team < ActiveRecord::Base
-  belongs_to :sec_team
+  #self join within teams
+  has_one :teir1, class_name: "Team", foreign_key: "sec_team_id"
+  has_one :teir2, class_name: "Team", foreign_key: "ter_team_id"
+  belongs_to :sec_team, class_name: "Team"
+  belongs_to :ter_team, class_name: "Team"
+
+  #across users
+  has_many :subordinates, class_name: "Employee"
+  belongs_to :manager, class_name: "Employee"  
 end
