053234d159a976fa4da840b0466a3409670fb8fd
wolfieorama <wachirawamunyi@gmail.com>
2016-04-01 15:26:15 +0300

    added managed team to relationship

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,6 @@ class Employee < ActiveRecord::Base
   has_many :subordinates, class_name: "Employee", foreign_key: "manager_id"
   belongs_to :manager, class_name: "Employee"
 
-  belongs_to :team 
-  has_one :team, foreign_key: "manager_id"
+  belongs_to :team, foreign_key: "team_id"
+  has_one :managed_team, foreign_key: "manager_id", class_name: "Team"
 end
